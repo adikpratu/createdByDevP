161a7e9105d38d5209e79976fd7dced0d45e0845
Pratiksha <pratikshaadik70@gmail.com>
2025-07-14 17:52:20 +0530

    first commit

---
+++ BankServices-1/src/main/java/com/example/demo/BankServices1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankServices1Application.class, args);
+	}
+
+}
